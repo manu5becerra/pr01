461d56d0bc1569c88e68b1b879702eb67303df7c
Manuel Fernandez <manudomi65@gmail.com>
2020-09-23 12:43:33 +0200

    DELETE: Eliminamos el codigo de prueba

--- pr01/src/codigo/Main.java
+++ pr01/src/codigo/Main.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-//Manuel
+
 public class Main
 {
   public static void main(String[] args)
