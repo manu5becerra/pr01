e2449724fd3a329b95bf62fb4c367c16d2423ab6
Manuel Fernandez <manudomi65@gmail.com>
2020-09-23 13:17:43 +0200

    ADD:Abrir conexion desde Conexion.java

--- pr01/src/codigo/Main.java
+++ pr01/src/codigo/Main.java
@@ -4,18 +4,21 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import codigo.db.Conexion;
 
 public class Main
 {
   public static void main(String[] args)
   {
-    Connection connection = null;
+	  
+	Conexion ctx = Conexion.getInstance();
+	ctx.close();
+   
     try
     {
       // create a database connection
-      connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
-      Statement statement = connection.createStatement();
-      statement.setQueryTimeout(30);  // set timeout to 30 sec.
+      
+   Statement statement = ctx.getState();
 
       statement.executeUpdate("drop table if exists person");
       statement.executeUpdate("create table person (id integer, name string)");
--- pr01/src/codigo/db/Conexion.java
+++ pr01/src/codigo/db/Conexion.java
@@ -1,10 +1,21 @@
 package codigo.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Conexion {
 	
 	private static Conexion instance;
 
 	private Conexion() {
+		try {
+			connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
 	}
 
 	public static Conexion getInstance() {
@@ -13,4 +24,50 @@ public class Conexion {
 		}
 		return instance;
 	}
+	
+	private Connection connection = null;
+	
+	public Statement getState() throws SQLException {
+		 Statement statement = connection.createStatement();
+	     statement.setQueryTimeout(30);  // set timeout to 30 sec.
+	     
+	     return statement;
+	
+		}
+		
+	
+	public void close() {
+		 try
+	      {
+	        if(connection != null)
+	          connection.close();
+	      }
+	      catch(SQLException e)
+	      {
+	        // connection close failed.
+	        System.err.println(e.getMessage());
+	      }
+	}
+	
+	
+	public void insertar(String TABLA, String[] values) {
+		
+	}
+	
+	
+	public void select (String TABLA) {
+		
+	}
+	
+	
+	public void 
+	
 }
+
+
+
+
+
+
+
+
